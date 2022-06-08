0737662b17e56314b2d82c09f713bde20a33dc43
wooseok0727 <crash2@naver.com>
2022-06-09 01:55:46 +0900

    first

--- next.config.js
+++ next.config.js
@@ -1,6 +1,52 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  reactStrictMode: false,
+  i18n: {
+    locales: ['ko'],
+    defaultLocale: 'ko',
+  },
 
-module.exports = nextConfig
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    });
+
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/sounds/',
+          outputPath: 'static/sounds/',
+          name: '[name].[ext]',
+          esModule: false,
+        },
+      },
+    });
+
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/models',
+          outputPath: 'static/models/',
+        },
+      },
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
