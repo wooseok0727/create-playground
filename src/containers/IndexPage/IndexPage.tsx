f6e8cc05fdaa553ccec47ae5ed22457f3b455be4
wooseok0727 <crash2@naver.com>
2022-06-12 15:01:41 +0900

    folder

--- src/containers/IndexPage/IndexPage.tsx
+++ src/containers/IndexPage/IndexPage.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { Head } from 'seo/Head/Head';
 
 import * as S from './IndexPage.styles';
-import { Vaporwave3D } from 'components/canvas/Vaporwave/Vaporwave3D/Vaporwave3D';
+import { Vaporwave3D } from 'components/canvas/vaporwave/Vaporwave3D/Vaporwave3D';
 
 export default function IndexPage() {
   const hasVistedLanding = useUIStore(s => s.hasVistedLanding);
